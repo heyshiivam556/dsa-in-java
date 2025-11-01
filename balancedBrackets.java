da375141c3eb56449f3d5417299f8fea2042eba4
Shivam Kumar <heyshiivam556@gmail.com>
2025-11-01 12:37:59 +0530

    solved balanced brackets

--- balancedBrackets.java
+++ balancedBrackets.java
@@ -3,9 +3,40 @@ import java.util.Stack;
 public class balancedBrackets {
 
     public static void main(String[] args) {
-        String exp = "[{{(a+b)+(c+d)})]";
-
-        Stack<Character> stc = new Stack<>();
-        int num = 34;
+        String exp1 = "()[]{[]}";
+        Stack<Character> stk = new Stack<>();
+        
+        for (int i = 0; i < exp1.length(); i++) {
+            char ch = exp1.charAt(i);
+            if (ch=='(' ||ch=='{' || ch=='[') {
+            stk.push(ch);
+           // System.out.println(stk.peek());
+            }else{
+                if (ch==')') {
+                    if (stk.peek()=='(') {
+                        stk.pop();
+                    }else{
+                        System.out.println(false);
+                        return;
+                    }
+                }else if (ch=='}') {
+                    if (stk.peek()=='{') {
+                        stk.pop();
+                    }else{
+                        System.out.println(false);
+                        return;
+                    }
+                }else if (ch==']') {
+                    if (stk.peek()=='[') {
+                        stk.pop();
+                    }else{
+                        System.out.println(false);
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println(true);
+            
     }
 }
