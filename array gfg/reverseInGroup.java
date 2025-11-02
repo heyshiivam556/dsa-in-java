9674257df9f8549a701faa7f3520cefd0cd31825
Shivam Kumar <heyshiivam556@gmail.com>
2025-11-02 10:42:46 +0530

    tried again the array reverse

--- array gfg/reverseInGroup.java
+++ array gfg/reverseInGroup.java
@@ -1,13 +1,21 @@
 public class reverseInGroup {
+    public static int[] reverseArray(int[] arr,int initial ,int finall){
+       
+        for (int i = initial; i < finall/2; i++) {
+        int temp = arr[initial];
+        arr[initial] = arr[finall-initial];
+        arr[finall] = temp;
+    }
+    return arr;
+    }
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8};
-        
-        for (int i = 1; i <= arr.length; i++) {
-            if (i%3==0) {
-                int temp = arr[i];
-                arr[i] = arr[i+2];
-                arr[i+2] = temp;
-            }
+    int[] arr = {1,2,3,4,5,6,7,8};
+    int set = 3;
+       for(int i=1;i<=arr.length;i++){
+        if (i%set==0) {
+            reverseArray(arr, i-set+1, i);
         }
+       } 
+       
     }
 }
