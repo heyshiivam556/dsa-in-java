db80df9fe8394a1e5083ff9f3c4dd7e7f5ae1c4f
Shivam Kumar <heyshiivam556@gmail.com>
2025-11-02 11:34:14 +0530

    not saved yet

--- nxtGrtrElmnt.java
+++ nxtGrtrElmnt.java
@@ -1,5 +1,15 @@
 public class nxtGrtrElmnt {
     public static void main(String[] args) {
-        
+        int[] arr = {2,4,5,9,6,12,8,11,3,1,7};
+        for (int i = 0; i < arr.length; i++) {
+           for (int j = i+1; j < arr.length-i; j++) {
+            if (arr[i]<arr[j]) {
+                System.out.println(arr[i]+" < "+arr[j]);
+                return;
+            }else{
+                System.out.println(arr[i]+" < "+"-1");
+            }
+           }
+        }
     }
 }
