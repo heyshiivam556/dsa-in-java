82860c088671e690a6f3af891b48f28d4d54f8b4
Shivam Kumar <heyshiivam556@gmail.com>
2025-11-09 10:53:57 +0530

    hey

--- array gfg/rotateArray.java
+++ array gfg/rotateArray.java
@@ -17,3 +17,31 @@ public class rotateArray {
         }
     }
 }
+/* 
+import java.util.Arrays;
+
+public class RotateArrayEfficient {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int d = 2;
+
+        d = d % arr.length; // Handle d > n
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, arr.length - 1);
+        reverse(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+*/
